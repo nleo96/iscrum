ccd72292652ea09f749fc5a67b7b6e0cc8786a75
nleo96 <44102025+nleo96@users.noreply.github.com>
2021-10-28 13:22:27 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/iscrum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iscrum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'source/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
